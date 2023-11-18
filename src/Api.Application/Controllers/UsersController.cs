16f83e8da62a13bed419aac4e680c1b8b07b92af
Lucas Queiroz <lucasestevam365@gmail.com>
2023-11-18 17:26:19 -0300

    Created user controller and adding dependency injection

---
+++ src/Api.Application/Controllers/UsersController.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Net;
+using System.Threading.Tasks;
+using Api.Domain.Interfaces.Services.User;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace Api.Application.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult> GetAll([FromServices] IUserService service)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                return Ok(await service.GetAll());
+            }
+            catch (ArgumentException e)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, e.Message);
+            }
+        }
+    }
+}
