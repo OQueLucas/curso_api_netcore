b266d4500e2e0a4468a7bc575eee2b48d04d5c66
Lucas Queiroz <lucasestevam365@gmail.com>
2023-11-20 01:23:27 -0300

    Created Authentication Button in Swagger

--- src/Api.Application/Startup.cs
+++ src/Api.Application/Startup.cs
@@ -1,19 +1,14 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Api.CrossCutting.DependencyInjection;
 using Api.Domain.Security;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Microsoft.OpenApi.Models;
 
@@ -66,26 +61,46 @@ namespace application
             });
 
             services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
                 {
-                    c.SwaggerDoc("v1", new OpenApiInfo
+                    Version = "v1",
+                    Title = "Curso de API com AspNetCore 3.1 - Na Prática",
+                    Description = "Arquitetura DDD",
+                    TermsOfService = new Uri("http://www.mfrinfo.com.br"),
+                    Contact = new OpenApiContact
                     {
-                        Version = "v1",
-                        Title = "Curso de API com AspNetCore 3.1 - Na Prática",
-                        Description = "Arquitetura DDD",
-                        TermsOfService = new Uri("http://www.mfrinfo.com.br"),
-                        Contact = new OpenApiContact
-                        {
-                            Name = "Marcos Fabrício Rosa",
-                            Email = "mfr@mail.com",
-                            Url = new Uri("http://wwww.mfrinfo.com.br")
-                        },
-                        License = new OpenApiLicense
-                        {
-                            Name = "Termo de Licença de Uso",
-                            Url = new Uri("http://wwww.mfrinfo.com.br")
-                        }
-                    });
+                        Name = "Marcos Fabrício Rosa",
+                        Email = "mfr@mail.com",
+                        Url = new Uri("http://wwww.mfrinfo.com.br")
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "Termo de Licença de Uso",
+                        Url = new Uri("http://wwww.mfrinfo.com.br")
+                    }
                 });
+
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = "Enter with JWT Token",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme {
+                            Reference = new OpenApiReference {
+                                Id = "Bearer",
+                                Type = ReferenceType.SecurityScheme
+                            }
+                        }, new List<string>()
+                    }
+                });
+            });
 
             services.AddControllers();
         }
